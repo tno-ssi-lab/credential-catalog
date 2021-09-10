c770cee674a226bbb6e159c82377fab046d7ccb3
Marnix <dev@videoparty.net>
2021-09-10 10:59:05 +0200

    First version of card

--- src/components/search/ResultCard.vue
+++ src/components/search/ResultCard.vue
@@ -1,26 +1,48 @@
 <template>
   <v-card outlined>
-    <v-card-title>{{credential.name}}</v-card-title>
+    <div class="top-container">
+      <v-card-title>
+        <router-link :to="{ name: 'details', params: { id: credential.id } }">
+          {{ credential.name }}
+        </router-link>
+      </v-card-title>
+      <v-card-subtitle>
+        from
+        <router-link :to="{ name: 'details', params: { id: credential.id } }">
+          {{ credential.name }}
+        </router-link>
+      </v-card-subtitle>
+
+      <!--
+      <v-btn
+        v-if="bundleCredentials.includes(credential)"
+        icon
+        @click="addToBundle(credential.id)"
+      >
+        <v-icon>mdi-bookmark-outline</v-icon>
+      </v-btn>
+      <v-btn
+        v-if="!bundleCredentials.includes(credential)"
+        icon
+        @click="removeFromBundle(credential.id)"
+      >
+        <v-icon>mdi-bookmark-check</v-icon>
+      </v-btn>
+     -->
+    </div>
     <v-card-text>
-      {{credential.description}}
+      {{ credential.description }}
     </v-card-text>
-    <v-btn v-if="bundleCredentials.includes(credential)"
-            icon
-            @click="addToBundle(credential.id)"
-    >
-      <v-icon>mdi-bookmark-outline</v-icon>
-    </v-btn>
-    <v-btn v-if="!bundleCredentials.includes(credential)"
-           icon
-           @click="removeFromBundle(credential.id)"
-    >
-      <v-icon>mdi-bookmark-check</v-icon>
-    </v-btn>
+
+    <v-card-actions>
+      <v-btn text :to="{ name: 'details', params: { id: credential.id } }">
+        See details
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
-
 import { mapMutations, mapGetters } from "vuex"
 
 export default {

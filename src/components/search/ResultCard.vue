aa8ee4eb191ac470e61957fbf4c60f444de0cdd2
Peter Langenkamp <peter.langenkamp@tno.nl>
2021-09-13 01:08:41 -0700

    fix bookmarking/bundling functionality

--- src/components/search/ResultCard.vue
+++ src/components/search/ResultCard.vue
@@ -13,22 +13,22 @@
         </router-link>
       </v-card-subtitle>
 
-      <!--
+      
       <v-btn
-        v-if="bundleCredentials.includes(credential)"
+        v-if="!bundleCredentials.includes(credential)"
         icon
         @click="addToBundle(credential.id)"
       >
         <v-icon>mdi-bookmark-outline</v-icon>
       </v-btn>
       <v-btn
-        v-if="!bundleCredentials.includes(credential)"
+        v-if="bundleCredentials.includes(credential)"
         icon
         @click="removeFromBundle(credential.id)"
       >
         <v-icon>mdi-bookmark-check</v-icon>
       </v-btn>
-     -->
+    
     </div>
     <v-card-text>
       {{ credential.description }}

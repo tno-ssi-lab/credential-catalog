b0cd7f1ca9584b2a9d5054dbcbd398e47b04f0dc
Marnix <dev@videoparty.net>
2021-09-24 13:10:28 +0200

    Added bookmark button

---
+++ src/components/common/BookmarkButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-btn
+      v-if="!bundleCredentials.includes(credential)"
+      icon
+      @click="addToBundle(credential.id)"
+    >
+      <v-icon>mdi-bookmark-outline</v-icon>
+    </v-btn>
+    <v-btn
+      v-if="bundleCredentials.includes(credential)"
+      icon
+      @click="removeFromBundle(credential.id)"
+    >
+      <v-icon>mdi-bookmark-check</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from "vuex"
+
+export default {
+  name: "BookmarkButton",
+  props: {
+    credential: {
+      default: null,
+      type: Object,
+    },
+  },
+  computed: {
+    ...mapGetters(["bundleCredentials"]),
+  },
+  methods: {
+    ...mapMutations(["addToBundle", "removeFromBundle"]),
+  },
+}
+</script>
